86aa164c184a59e81942ce0f9e6c7f073982e336
vlad <vlad.markuts@gmail.com>
2020-04-15 23:34:14 +0300

    completing todo2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 let colorGray = 'rgb(128, 128, 128)';
 let colorBlack = 'rgb(0, 0, 0)';
 let input = $('input[type="text"]');
-let plus = $('.fa-plus');
-
+let toggleInput = $('#toggleInput');
+let displayInput = true;
+let h1 = $('h1');
 
 $('ul').on('click', 'li', function(){
 	$(this).toggleClass('completed');
+	checkState();
 });
 
 //click on X to delete todo item
@@ -17,21 +19,16 @@ $('ul').on('click', 'span', function(event){
 	event.stopPropagation();
 });
 
+
 input.keypress(function(event){
 	if(event.which === 13) {
-		//grabb new todo from imput
 		let todoText = $(this).val();
-		//create new li
 		$('ul').append('<li><span><i class="fa fa-trash"></i></span> ' + todoText + '</li>');
-		//clear input
 		$(this).val('');
 	}
 });
 
-plus.click(function(){
-	input.fadeToggle();
+toggleInput.click(function(){
+	displayInput ? input.fadeOut() : input.fadeIn();
+	displayInput = !displayInput;
 });
-
-if (input.val().length > 25) {
-	input.slice(input.val().length-1);
-}
